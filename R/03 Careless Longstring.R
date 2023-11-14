09374d992dd7249fc57193d98b71d508cf503de7
Stephanie Towch <150716716+stephtowch@users.noreply.github.com>
2023-11-14 17:49:34 +0000

    Update and rename 03 Careless_Longstring.R to 03 Careless Longstring.R

--- R/03 Careless Longstring.R
+++ R/03 Careless Longstring.R
@@ -1,9 +1,9 @@
-####################Careless Participants#############################
+####################CARELESS PARTICIPANTS#############################
 #Step 0: Load the dataset
 
 options(scipen = 999)
 options(max.print = 1000000)
-data = readr::read_csv("C:/Users/Steph/Documents/careless_data.csv")
+data = readr::read_csv("data/data.csv")
 View(data)
 
 
@@ -23,7 +23,7 @@ boxplot(careless_long)
 longstring = data.frame(careless_long)
 names(longstring)[names(longstring) == "careless_long"] = "LST" #renames column LST
 longstring$ID = seq.int(nrow(longstring)) #adds ID var for merging later
-write.csv(longstring, row.names = FALSE, "C:/Users/Steph/Documents/longstring.csv")
+write.csv(longstring, row.names = FALSE, "data/longstring.csv")
 View(longstring)
 
 #Step 3: IRV analysis
@@ -32,19 +32,19 @@ View(longstring)
 #View(irv_total)
 #boxplot(irv_total) ##produce a boxplot of the IRV for all quarters
 
-#write.csv(irv_total, "C:/Users/Halley Pontes/Desktop/irv.csv")
+#write.csv(irv_total, "data/data.csv")
 
 #Literature: https://doi.org/10.1007/s10869-016-9479-0 
 #The idea is that carelessness emerges when there's little variation in the SD (i.e., IRV..)
 
 #Step 4: Merge the datasets
 data2 = merge(data, longstring, by = "ID")
-write.csv(data2, "C:/Users/Steph/Documents/data2.csv", row.names = FALSE)
+write.csv(data2, "data/data2.csv", row.names = FALSE)
 View(data2)
 
 #Step 5: Exclude all careless_longstring participants
 table(data2$LST) #cutoff => 9.5: participants that answered invariantly on at least 50% of the study vars (9.5 vars)
 data3 = subset(data2, LST < 9.5) #Deletes CIE participants
 table(data3$LST)
-write.csv(data3, "C:/Users/Steph/Documents/data3.csv", row.names = FALSE)
+write.csv(data3, "data/data3.csv", row.names = FALSE)
 view(data3)
